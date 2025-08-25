970fccb0db1609e2216db0e352441906940289f1
shashidhar078 <nagunurishashidhar@gmail.com>
2025-08-26 01:42:00 +0530

    implemented navbar

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ async function main(){
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsmate);
